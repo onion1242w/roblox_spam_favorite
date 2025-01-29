fd8c92208c97e0bf5989427f60a529b63528a76d
onion <borissimonenko222@gmail.com>
2025-01-29 18:11:26 +0300

    Fixed asdasdaratr

---
+++ build/CMakeFiles/Gen.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gen.dir/main.cpp.obj"
+  "CMakeFiles/Gen.dir/main.cpp.obj.d"
+  "Gen.exe"
+  "Gen.exe.manifest"
+  "Gen.pdb"
+  "libGen.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Gen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Gen.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Gen.
+# This may be replaced when dependencies are built.
